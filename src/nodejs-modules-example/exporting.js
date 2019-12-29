7761e395328948c36fef7feffa65e9a700332e82
Daniel Faderski <daniel.faderski@allegro.pl>
2019-12-30 00:05:42 +0100

    nodejs modules

---
+++ src/nodejs-modules-example/exporting.js
@@ -0,0 +1,11 @@
+const user = {
+  username: 'Janusz',
+};
+
+const printJanusz = function() {
+  console.log(user.username);
+};
+
+exports.user = user;
+exports.printJanusz = printJanusz;
+
---
+++ src/nodejs-modules-example/importing.js
@@ -0,0 +1,9 @@
+const {user, printJanusz} = require('./exporting');
+
+const userUtils = require('./exporting');
+
+console.log(userUtils.user);
+console.log(user);
+printJanusz();
+
+

fe211496fe373a674c78c6fce406e44b11eed454
Daniel Faderski <daniel.faderski@allegro.pl>
2020-01-01 21:42:32 +0100

    html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -15,4 +16,7 @@ module.exports = {
       },
     ],
   },
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html'
+  })]
 };

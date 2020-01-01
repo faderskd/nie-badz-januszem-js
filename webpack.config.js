2a37eedbe3c229e73377dedc7eba1afc34ab4051
Daniel Faderski <daniel.faderski@allegro.pl>
2020-01-01 21:26:31 +0100

    css and style loaders

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,15 @@ module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "index.js",
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
 };

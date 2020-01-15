a61d84b85388aa0c3d93b0a4dec7a9779b1f2d39
Daniel Faderski <daniel.faderski@allegro.pl>
2020-01-15 01:59:02 +0100

    Rewrite integration test to use async functions

--- tests/fetch-tasks.integration.spec.js
+++ tests/fetch-tasks.integration.spec.js
@@ -9,14 +9,15 @@ chai.use(chaiFetch);
 const {expect} = chai;
 
 describe('async/await functions in js', () => {
-  beforeEach(() => mockServer.start(9000));
-  afterEach(() => mockServer.stop());
+  beforeEach(async () => {
+    await mockServer.start(9000);
+  });
+  afterEach(async () => await mockServer.stop());
 
-  it('should fetch tasks to do', () =>
+  it('should fetch tasks to do', async () => {
     // eslint-disable-next-line max-len
-    mockServer.get('/tasks')
-        .thenReply(200, JSON.stringify({tasks: ['learn js', 'learn webpack']}))
-        .then(() => fetchTasksToDo('http://localhost:9000/tasks')
-        // eslint-disable-next-line max-len
-            .then((tasks) => expect(tasks).to.include.members(['learn js', 'learn webpack']))));
+    await mockServer.get('/tasks').thenReply(200, JSON.stringify({tasks: ['learn js', 'learn webpack']}));
+    const tasks = await fetchTasksToDo('http://localhost:9000/tasks');
+    expect(tasks).to.include.members(['learn js', 'learn webpack']);
+  });
 });
